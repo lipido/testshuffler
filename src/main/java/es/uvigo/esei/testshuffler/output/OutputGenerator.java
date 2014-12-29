d0646ea3f9234e2b158d9e9c40c91d08b51a7e91
lipido <dgpena@uvigo.es>
2014-12-29 01:41:40 +0100

    Add basic output generation classes.

---
+++ src/main/java/es/uvigo/esei/testshuffler/output/OutputGenerator.java
@@ -0,0 +1,8 @@
+package es.uvigo.esei.testshuffler.output;
+
+/**
+ * Created by lipido on 12/28/14.
+ */
+public interface OutputGenerator<T> {
+    public T generateOutput();
+}
---
+++ src/main/java/es/uvigo/esei/testshuffler/output/PDFOutput.java
@@ -0,0 +1,12 @@
+package es.uvigo.esei.testshuffler.output;
+
+/**
+ * Created by lipido on 12/28/14.
+ */
+public class PDFOutput implements OutputGenerator<byte[]> {
+
+    @Override
+    public byte[] generateOutput() {
+        return null;
+    }
+}
